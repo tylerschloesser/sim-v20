3ae4994caea0fb82e525536f04c6641dd27578c9
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-11 06:47:52 -0700

    add container

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import { StrictMode } from 'react'
+import clsx from 'clsx'
+import { StrictMode, useEffect, useRef } from 'react'
 import { createRoot } from 'react-dom/client'
 import invariant from 'tiny-invariant'
 import './index.css'
@@ -8,6 +9,27 @@ invariant(container)
 
 createRoot(container).render(
   <StrictMode>
-    <>TODO</>
+    <App />
   </StrictMode>,
 )
+
+function App() {
+  const container = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    invariant(container.current)
+  }, [])
+
+  return (
+    <div
+      className={clsx(
+        'w-20 h-20',
+        'bg-red-500 border-2 border-black',
+        'cursor-pointer',
+      )}
+      ref={container}
+    >
+      TODO
+    </div>
+  )
+}
