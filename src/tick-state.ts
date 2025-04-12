47208bb7626029d868d1ffee57bdbb23c49105c2
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-12 08:17:19 -0700

    decrease energy

--- src/tick-state.ts
+++ src/tick-state.ts
@@ -5,6 +5,11 @@ import { entityPositionToId, onVisitEntity } from './util'
 export function tickState(draft: AppState): void {
   draft.tick += 1
 
+  invariant(draft.player.energy >= 0)
+  if (draft.player.energy === 0) {
+    return
+  }
+
   const playerEntity =
     draft.entities[
       entityPositionToId(draft.player.position)
@@ -13,8 +18,12 @@ export function tickState(draft: AppState): void {
 
   switch (playerEntity.type) {
     case 'undiscovered': {
+      invariant(playerEntity.ticksRemaining > 0)
+
       playerEntity.ticksRemaining -= 1
-      if (playerEntity.ticksRemaining <= 0) {
+      draft.player.energy -= 1
+
+      if (playerEntity.ticksRemaining === 0) {
         const updatedEntity = (draft.entities[
           playerEntity.id
         ] = {
