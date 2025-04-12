6c5caf867ac5bba48214249aa2c6c7e2fd96b99f
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-12 07:19:09 -0700

    add undiscovered entities

--- src/init-state.ts
+++ src/init-state.ts
@@ -1,7 +1,5 @@
-import { isInteger } from 'lodash-es'
-import invariant from 'tiny-invariant'
-import { AppState, Entity } from './types'
-import { entityPositionToId } from './util'
+import { AppState } from './types'
+import { addEntity } from './util'
 import { Vec2 } from './vec2'
 
 export function initState(): AppState {
@@ -17,35 +15,27 @@ export function initState(): AppState {
     entities: {},
   }
 
-  function addEntity(partial: Omit<Entity, 'id'>): void {
-    invariant(isInteger(partial.position.x))
-    invariant(isInteger(partial.position.y))
-    const id = entityPositionToId(partial.position)
-    invariant(!state.entities[id])
-    state.entities[id] = { ...partial, id }
-  }
-
-  addEntity({
+  addEntity(state, {
     type: 'root',
     position: Vec2.ZERO,
     size: new Vec2(1, 1),
   })
-  addEntity({
+  addEntity(state, {
     type: 'node',
     position: new Vec2(1, 0),
     size: new Vec2(1, 1),
   })
-  addEntity({
+  addEntity(state, {
     type: 'node',
     position: new Vec2(0, 1),
     size: new Vec2(1, 1),
   })
-  addEntity({
+  addEntity(state, {
     type: 'node',
     position: new Vec2(-1, 0),
     size: new Vec2(1, 1),
   })
-  addEntity({
+  addEntity(state, {
     type: 'node',
     position: new Vec2(0, -1),
     size: new Vec2(1, 1),
--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,21 @@
-import { AppState } from './types'
+import { isInteger } from 'lodash-es'
+import invariant from 'tiny-invariant'
+import { AppState, Entity } from './types'
 import { Vec2 } from './vec2'
 
 export function entityPositionToId(position: Vec2): string {
   return `${position.x},${position.y}`
+}
+
+export function addEntity(
+  state: AppState,
+  partial: Omit<Entity, 'id'>,
+): void {
+  invariant(isInteger(partial.position.x))
+  invariant(isInteger(partial.position.y))
+  const id = entityPositionToId(partial.position)
+  invariant(!state.entities[id])
+  state.entities[id] = { ...partial, id }
 }
 
 export function move(draft: AppState, delta: Vec2): void {
@@ -11,7 +24,24 @@ export function move(draft: AppState, delta: Vec2): void {
     draft.player.position,
   )
   const targetEntity = draft.entities[targetEntityId]
-  if (targetEntity) {
-    draft.player.position = targetEntity.position
+  invariant(targetEntity)
+
+  for (const delta of [
+    new Vec2(0, 1),
+    new Vec2(1, 0),
+    new Vec2(0, -1),
+    new Vec2(-1, 0),
+  ]) {
+    const neighborPosition =
+      targetEntity.position.add(delta)
+    const neighborId = entityPositionToId(neighborPosition)
+    const neighborEntity = draft.entities[neighborId]
+    if (!neighborEntity) {
+      addEntity(draft, {
+        type: 'undiscovered',
+        position: neighborPosition,
+        size: new Vec2(1, 1),
+      })
+    }
   }
 }
