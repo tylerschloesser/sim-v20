e766d7e3fc6409750755e30661236bd50e99ff1d
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-12 17:49:20 -0700

    refactor translate

--- src/app.tsx
+++ src/app.tsx
@@ -74,26 +74,29 @@ function TickComponent() {
 }
 
 export function WorldComponent() {
+  const container = useRef<HTMLDivElement>(null)
   const { state } = useContext(AppContext)
   const entityIds = Object.keys(state.entities)
   const robotIds = Object.keys(state.robots)
 
   const { cursor } = state
 
-  const style = useMemo<React.CSSProperties>(() => {
-    const { x, y } = cursor.position.mul(
+  const translate = useMemo(() => {
+    return cursor.position.mul(
       state.scale * state.spread * -1,
     )
-    return {
-      translate: `${x}px ${y}px`,
-    }
   }, [cursor.position, state.scale, state.spread])
+
+  useEffect(() => {
+    invariant(container.current)
+    container.current.style.translate = `${translate.x}px ${translate.y}px`
+  }, [translate])
 
   return (
     <div
       className={clsx('absolute inset-0 overflow-hidden')}
     >
-      <div className={clsx('absolute')} style={style}>
+      <div ref={container} className={clsx('absolute')}>
         {robotIds.map((robotId) => (
           <RobotComponent key={robotId} robotId={robotId} />
         ))}
