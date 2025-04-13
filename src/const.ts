db767f8786cfed11a102ebc52c7a83082f764cc1
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-12 17:11:11 -0700

    fix constant

--- src/const.ts
+++ src/const.ts
@@ -2,7 +2,7 @@ export const TICK_DURATION = 100
 
 export const SPREAD = 1.5
 
-export const DISCOVERY_CONSTANT = 3
+export const DISCOVERY_CONSTANT = 1.5
 export const DISCOVERY_EXPONENT = 2.5
 
 export const MAX_PLAYER_ENERGY = 1000
