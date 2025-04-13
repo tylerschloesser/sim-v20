b07d8a6eb2b77823a3e6e2cefabc7cf600f3d5ba
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-12 19:40:53 -0700

    finish smooth

--- src/world-component.tsx
+++ src/world-component.tsx
@@ -53,6 +53,34 @@ export function WorldComponent() {
   )
 }
 
+function smooth(
+  current: Vec2,
+  target: Vec2,
+  dt: number,
+): Vec2 {
+  if (current.equals(target)) {
+    return current
+  }
+
+  const d = target.sub(current)
+  const len = d.length()
+
+  if (len < 0.01) {
+    return target
+  }
+
+  const v = d
+    .normalize()
+    .mul((len + 1) ** 1.4 - 1)
+    .mul(0.8)
+
+  if (v.mul(dt).length() > len) {
+    return target
+  }
+
+  return current.add(v.mul(dt))
+}
+
 function useSmooth(
   container: RefObject<HTMLDivElement | null>,
   target: RefObject<Vec2>,
@@ -63,17 +91,13 @@ function useSmooth(
     let handle: number
     const callback: FrameRequestCallback = () => {
       const now = self.performance.now()
-      // @ts-expect-error
-      const dt = now - lastFrame
+      const dt = (now - lastFrame) / 1000
       lastFrame = now
 
-      if (!translate.equals(target.current)) {
-        // const d = target.current.sub(translate)
-        translate = target.current
+      translate = smooth(translate, target.current, dt)
 
-        invariant(container.current)
-        container.current.style.translate = `${translate.x}px ${translate.y}px`
-      }
+      invariant(container.current)
+      container.current.style.translate = `${translate.x}px ${translate.y}px`
 
       handle = self.requestAnimationFrame(callback)
     }
